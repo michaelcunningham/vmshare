a92c983ba5abf55f53506f3113b208ce16f2a6c0
Michael Cunningham <napacunningham@gmail.com>
2016-10-27 15:54:47 -0700

    Updating Thu Oct 27 15:54:47 PDT 2016

---
+++ userdata_light_test_queries.sql
@@ -0,0 +1,43 @@
+select  /* mu01 */ u.*, (split_part(ip_address, '.', 1)::bigint << 24)
+		+ (split_part(ip_address, '.', 2)::bigint << 16)
+		+ (split_part(ip_address, '.', 3)::bigint << 8)
+		+ (split_part(ip_address, '.', 4)::bigint) as ipInt,
+        0 as pets, cast(0 as numeric) as usd_amt,
+        0 as vip, 0 as pFunnel, 0 as badtag, cast(null as varchar(256)) as isp,
+        cast(null as varchar(256)) as ispdomain, cast(null as char(2)) as reg_iso,
+        0 as badisp, cast(null as varchar(100)) as photohash,
+        cast(null as varchar(256)) as tagline, cast(null as varchar(256)) as displayname,
+        0 as msgs, cast(null as varchar(256)) as ua    
+from    userdata_light u    
+where   date_registered::date>='2016-10-26';
+
+select  /* mu02 */ u.*, (split_part(ip_address, '.', 1)::bigint << 24)
+		+ (split_part(ip_address, '.', 2)::bigint << 16)
+		+ (split_part(ip_address, '.', 3)::bigint << 8)
+		+ (split_part(ip_address, '.', 4)::bigint) as ipInt,
+        0 as pets, cast(0 as numeric) as usd_amt,
+        0 as vip, 0 as pFunnel, 0 as badtag, cast(null as varchar(256)) as isp,
+        cast(null as varchar(256)) as ispdomain, cast(null as char(2)) as reg_iso,
+        0 as badisp, cast(null as varchar(100)) as photohash,
+        cast(null as varchar(256)) as tagline, cast(null as varchar(256)) as displayname,
+        0 as msgs, cast(null as varchar(256)) as ua    
+from    userdata_light_new u    
+where   date_registered::date>='2016-10-26';
+
+
+select  /* mlu01 */ *
+from    lh_galaxy l
+        join userdata_light u
+                on l.sender_user_id = u.user_id    
+where   ct >= 5
+and     gender = 'F'
+order by ct;
+
+select  /* mlu02 */ *
+from    lh_galaxy l
+        join userdata_light_new u
+                on l.sender_user_id = u.user_id    
+where   ct >= 5
+and     gender = 'F'
+order by ct;
+
